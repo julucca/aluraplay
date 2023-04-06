1340edb5291b53a5492f9131fbe671834fe37c18
Juliana Lucca <lucca.editora@gmail.com>
2023-04-06 15:20:47 -0300

    feat: implement search tool

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -18,12 +18,21 @@ async function criaVideo(titulo, descricao, url, imagem) {
             imagem: imagem
         })
     })
+
     const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
+
+async function buscaVideo(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
+    const conexaoConvertida = conexao.json();
 
     return conexaoConvertida;
 }
 
 export const conectaApi = {
     listaVideos,
-    criaVideo
+    criaVideo,
+    buscaVideo
 }
